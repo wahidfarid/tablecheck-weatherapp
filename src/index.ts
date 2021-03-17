080f7b058fc9f459732e655ece948968e0a71e91
Wahid Magdy <wahidmagdy@gmail.com>
2021-03-17 09:04:32 +0200

    Use Typescript
    Overwrite project with the create-razzle-app typescript boilerplate

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 let app = require('./server').default;
 
 if (module.hot) {
-  module.hot.accept('./server', function() {
+  module.hot.accept('./server', () => {
     console.log('🔁  HMR Reloading `./server`...');
     try {
       app = require('./server').default;
@@ -14,14 +14,10 @@ if (module.hot) {
   console.info('✅  Server-side HMR Enabled!');
 }
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
 export default express()
   .use((req, res) => app.handle(req, res))
-  .listen(port, function(err) {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(`> Started on port ${port}`);
+  .listen(port, () => {
+    console.log(`> App started http://localhost:${port}`)
   });
